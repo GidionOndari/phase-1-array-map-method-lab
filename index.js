f18067b04e3b6656a12be2997c2e89be159e28d2
ondarigidion <gidion.ondari@student.moringaschool.com>
2025-06-22 12:41:32 +0300

    done

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+/* original tutorials array here … */
+
+function titleCased() {
+  return tutorials.map(sentence => {
+    return sentence
+      .split(" ")
+      .map(word => word[0].toUpperCase() + word.slice(1)) // keep inner casing
+      .join(" ");
+  });
 }
+
